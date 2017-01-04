e56bbe42b74fc0b7d8244c2aebecd2ae071b0e10
Florent NOEL <florent.noel@drust.io>
2017-01-05 00:12:22 +0100

    Add a server to serve frontend
    Once built, the static files of the server can be served by this local
    simple server.

---
+++ frontend/server/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
